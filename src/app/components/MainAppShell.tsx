a0f2982ada5a0ce608a0f6c472a96ed0f8944ab7
Rashmika Fernando <sethumrashmika3001@gmail.com>
2024-10-25 23:08:10 +0530

    fix: minor fixes

--- src/app/components/MainAppShell.tsx
+++ src/app/components/MainAppShell.tsx
@@ -14,29 +14,29 @@ export default function MainAppShell({
   if (status === "loading") return <Loading />;
 
   // Private pages with sidebar (for authenticated users)
-  if (status === "authenticated")
-    return (
-      <AppShell>
-        <AppShell.Navbar>
-          <Sidebar />
-        </AppShell.Navbar>
-        <AppShell.Main>{children}</AppShell.Main>
-        <AppShell.Footer>
-          <p
-            style={{
-              textAlign: "center",
-              margin: 8,
-              fontSize: "small",
-              color: "grey"
-            }}
-          >
-            Made with ❤ by&ensp;<strong>&lt;/Dev.Team&gt;</strong>
-            <br />
-            of AIESEC in Sri Lanka
-          </p>
-        </AppShell.Footer>
-      </AppShell>
-    );
+
+  return (
+    <AppShell>
+      <AppShell.Navbar>
+        <Sidebar />
+      </AppShell.Navbar>
+      <AppShell.Main>{children}</AppShell.Main>
+      <AppShell.Footer>
+        <p
+          style={{
+            textAlign: "center",
+            margin: 8,
+            fontSize: "small",
+            color: "grey"
+          }}
+        >
+          Made with ❤ by&ensp;<strong>&lt;/Dev.Team&gt;</strong>
+          <br />
+          of AIESEC in Sri Lanka
+        </p>
+      </AppShell.Footer>
+    </AppShell>
+  );
 
   // Public pages (for unauthenticated users)
   return children;
